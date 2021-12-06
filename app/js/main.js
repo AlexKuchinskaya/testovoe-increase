921110d8eeb48eb275410ca1f749ef64648e0461
Alexandra <alexkuchynsk@gmail.com>
2021-12-06 12:22:02 +0100

    Finished header

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,18 @@
 'use strict';
 (function () {
-    const navToggle = document.querySelector('.header__toggle');
-    console.log(`navToggle`, navToggle)
-    const pageHeader = document.querySelector('.header');
-
-    pageHeader.classList.remove('header--nojs');
+    const navToggle = document.querySelector(".header__toggle");
+    const pageHeader = document.querySelector(".header");
+ 
+    pageHeader.classList.remove("header--nojs");
     navToggle.addEventListener("click", function() {
-        if (pageHeader.classList.contains('header--closed')) {
-            pageHeader.classList.remove('header--closed');
-            pageHeader.classList.add('header--opened');
-
-        } else {
-            pageHeader.classList.add('header--closed');
-            pageHeader.classList.romve('header--opened');
-        }
-    })
+        console.log('hello')
+      if (pageHeader.classList.contains("header--closed")) {
+        console.log('hello§')
+        pageHeader.classList.remove("header--closed");
+        pageHeader.classList.add("header--opened");
+      } else {
+        pageHeader.classList.add("header--closed");
+        pageHeader.classList.remove("header--opened");
+      }
+    });
 })();
